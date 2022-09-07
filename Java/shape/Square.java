b9637d4821fcc803d2aacb11b0b042da01bfdfdb
Manisha <98947237+manishaachar@users.noreply.github.com>
2022-09-07 20:06:43 +0530

    .

---
+++ Java/shape/Square.java
@@ -0,0 +1,14 @@
+package shape;
+
+public class Square {
+    float side;
+    public void getData(float temp)
+    {
+        side=temp;
+    }
+    public double area()
+    {
+        return(side*side);
+    }
+    
+}
